c1fc11aa3e93720724f200c71b52f7d8350685fa
abdul aziz <atmberjalan2017@gmail.com>
2017-07-20 16:57:55 +0700

    penambahan swipe agenActivity dan Layout
    penambahan swipe agenActivity dan Layout

--- app/src/main/java/id/co/jmx/jmxperience/AgenActivity.java
+++ app/src/main/java/id/co/jmx/jmxperience/AgenActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -12,7 +14,7 @@ import android.webkit.WebView;
 import id.co.jmx.jmxperience.Helper.AksesManager;
 
 public class AgenActivity extends DasarActivity {
-
+    private SwipeRefreshLayout swipeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +37,28 @@ public class AgenActivity extends DasarActivity {
                 WebSettings webSettings = myWebView.getSettings();
                 webSettings.setJavaScriptEnabled(true);
             }
-
+            swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+            swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+                @Override
+                public void onRefresh(){
+                    refreshContent();
+                }
+            });
         }else{
             Intent i = new Intent(c, LoginActivity.class);
             finish();
             startActivity(i);
         }
+    }
+    public void refreshContent() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+                swipeLayout.setRefreshing(false);
+            }
+        }, 100);
     }
 }
